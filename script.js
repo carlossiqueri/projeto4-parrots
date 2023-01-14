150dc771cc34767f5cc910b134f50e416e41bfdc
Carlos Siqueri <carlossiqueri@Carlos>
2023-01-14 16:48:21 -0300

    Versão que rotaciona as cartas ao clique

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
 let pares = 0;
 let opcoesGif = [
-  './img/bobrossparrot.gif',
-  './img/explodyparrot.gif',
-  './img/fiestaparrot.gif',
-  './img/metalparrot.gif',
-  './img/revertitparrot.gif',
-  './img/tripletsparrot.gif',
-  './img/unicornparrot.gif'
-]
+  "./img/bobrossparrot.gif",
+  "./img/explodyparrot.gif",
+  "./img/fiestaparrot.gif",
+  "./img/metalparrot.gif",
+  "./img/revertitparrot.gif",
+  "./img/tripletsparrot.gif",
+  "./img/unicornparrot.gif",
+];
 function modoJogo() {
-    pares = prompt('Com quantas cartas você deseja jogar? (números pares entre 4 e 14)');
-    pares = Number(pares);
+  pares = prompt(
+    "Com quantas cartas você deseja jogar? (números pares entre 4 e 14)"
+  );
+  pares = Number(pares);
 }
 modoJogo();
 
-while (pares < 4 || pares%2 !== 0 || pares > 14) {
-        modoJogo();
-    }
+while (pares < 4 || pares % 2 !== 0 || pares > 14) {
+  modoJogo();
+}
 
-//randomizador 
-function comparador() { 
-	return Math.random() - 0.5; 
+//randomizador
+function comparador() {
+  return Math.random() - 0.5;
 }
 
 let deck = [];
-const numPares = Number(pares/2);
-for (let i = 0; i < numPares; i++){
+const numPares = Number(pares / 2);
+for (let i = 0; i < numPares; i++) {
   deck.push(opcoesGif[i]);
   deck.push(opcoesGif[i]);
 }
-
-console.log(deck);
-
+// Randomiza as cartas de acordo com a quantidade selecionada pelo usuario
 deck.sort(comparador);
 
 let imgJogo;
 
-function addImg () {
-    for(let i = 0; i < pares; i++) {
-        imgJogo = document.querySelector('main');
-    imgJogo.innerHTML += `<div class="card">
-    <div class="front-face face" onclick="rotacionaCarta(this)">
-      <img class="imgVerso" src="./img/back.png" alt="">
-    </div>
-    <div class="back-face face">
-      <img class="gifReveal" src="${deck[i]}" alt="">
-    </div>
-  </div>`
-}
+// adiciona as cartas na tela
+function addImg() {
+  for (let i = 0; i < pares; i++) {
+    imgJogo = document.querySelector("main");
+    let cardAdd = `
+    <div class="card" onclick="rotacionaCarta(this)">
+      <div class="front-face face">
+        <img class="imgVerso" src="./img/back.png" alt="">
+      </div>
+      <div class="back-face face">
+        <img class="gifReveal" src="${deck[i]}" alt="">
+      </div>
+    </div>`;
+
+  imgJogo.innerHTML += cardAdd;
+  }
+  
 }
 
-addImg ();
+addImg();
+
+function rotacionaCarta(cardRevelado) {
+  const revelado = cardRevelado.querySelector('.front-face');
+  const escondido = cardRevelado.querySelector('.back-face');
+    
+  const rotacionado = cardRevelado.querySelectorAll('.front-face .cardRotacionado');
+  console.log(rotacionado);
+    // se uma ou nenhuma carta foi virada e se o elemento clicado não foi virado ainda{}
+    revelado.classList.add('cardRotacionado');
+    escondido.classList.add('gifRevelado');
+
+}
 
